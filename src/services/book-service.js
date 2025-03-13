27c82fde2fd9099987d936f1a4c0ba381d36e39c
lauraM84 <laura.mazza84@gmail.com>
2025-03-13 13:38:25 +0100

    first commit

---
+++ src/services/book-service.js
@@ -0,0 +1,22 @@
+
+export default class BookService {
+
+    constructor() {
+    }
+
+    getBooksData() {
+
+        const booksPromise = fetch("/assets/books.json")
+            .then(resp => resp.json())
+            .then(jsonData => {
+
+                console.log(jsonData);
+
+            })
+            .catch(error => console.log(error))
+
+
+    }
+}
+
+
